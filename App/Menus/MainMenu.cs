6b5cde2abc7bdf528e19e0aa128b52d5ec41f32a
SabrinaSocarras <sabrinasocarras2006@gmail.com>
2025-01-30 21:43:45 -0500

    Main Menu mejorado

--- App/Menus/MainMenu.cs
+++ App/Menus/MainMenu.cs
@@ -6,7 +6,17 @@ public static class MainMenu
     public static void ShowMenu()
     {
         // "Barbie" title in large and pink
-        AnsiConsole.MarkupLine("[bold pink1]Welcome to Barbie's Maze[/]");
+       AnsiConsole.Write(
+            new FigletText("Welcome  to  Barbie's  Secret  Garden")
+                .Centered()
+                .Color(Color.HotPink)
+        );
+
+        var panel = new Panel("This is a fun competition where you have to reach the tulip flower to win the game. You will have to avoid the traps and obstacles that you will find on the way. Each player will have a special ability that will help you to reach the goal. Good luck!")
+            .Header("[bold underline white]About[/]")
+            .Border(BoxBorder.Double)
+            .BorderStyle(new Style(Color.SeaGreen2));
+        AnsiConsole.Write(panel);
 
         // Menu options
         var option = AnsiConsole.Prompt(
--- Program.cs
+++ Program.cs
@@ -7,6 +7,36 @@ public static class Program
 {
     static void Main(string[] args)
     {
+     
+        // Frecuencias para las notas musicales
+        int doNote = 262;  // Do
+        int reNote = 294;  // Re
+        int miNote = 330;  // Mi
+        int solNote = 392; // Sol
+        int laNote = 440;  // La
+        int siNote = 494;  // Si
+
+        
+        // Melodía alegre y mágica
+         Console.Beep(miNote, 500); // Mi
+        Console.Beep(solNote, 500); // Sol
+        Console.Beep(laNote, 500); // La
+        Console.Beep(miNote, 500); // Mi
+        Console.Beep(solNote, 500); // Sol
+        Console.Beep(laNote, 500); // La
+        Console.Beep(siNote, 500); // Si
+        Console.Beep(laNote, 500); // La
+        Console.Beep(solNote, 500); // Sol
+        Console.Beep(miNote, 500); // Mi
+        Console.Beep(reNote, 500); // Re
+        Console.Beep(doNote, 500); // Do
+        Console.Beep(reNote, 500); // Re
+        Console.Beep(miNote, 500); // Mi
+        Console.Beep(miNote, 500); // Mi
+        Console.Beep(reNote, 1000); // Re (más largo)
+        Console.Beep(doNote, 1000);
+        
+
         MainMenu.ShowMenu();
         GameMenu gameMenu = new GameMenu();
         Application app = new Application();
